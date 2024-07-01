672744ab049934e87c9eec5670ab17fd1b4ce85e
Ola Jaafreh <olamahmoudj@yahoo.com>
2024-07-01 08:03:26 +0300

    Book2

--- Java Spring/Spring MVC/Books2/src/main/java/com/example/Books2/controller/BookController.java
+++ Java Spring/Spring MVC/Books2/src/main/java/com/example/Books2/controller/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Books2.models.Book;
 import com.example.Books2.services.Services;
@@ -13,8 +14,13 @@ import com.example.Books2.services.Services;
 
 @Controller
 public class BookController {
-    @Autowired
-    private Services bookService;
+//    @Autowired
+//    private Services bookService;
+    
+        private final Services bookService;
+        public BookController(Services bookService){
+            this.bookService = bookService;
+        }
 
     @RequestMapping("/books/{id}")
     public String show(@PathVariable("id") Long id, Model model) {
